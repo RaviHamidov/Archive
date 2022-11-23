5cda114499f6c1f9f8bc5f85b51ffb4ca570445b
guseinguseinov <gusein.guseinov2002@gmail.com>
2022-11-23 10:37:01 +0400

    again fixed mezun fikirleri

--- js/swipers/mezunFikirleriSwiper.js
+++ js/swipers/mezunFikirleriSwiper.js
@@ -28,14 +28,14 @@ mezunFikirData.forEach((data) => {
   mezunFikirleriWrapper.innerHTML += `
         <div class="swiper-slide flex flex-col md:flex-row md:p-9">
           <img class="w-full md:w-2/4" src="${data.scr}" alt="bombee">
-          <div class="w-2/4 flex flex-col justify-center mx-auto pl-6 pr-20">       
-            <p class="text-xl md:text-6xl mb-6 text-[#1A265C]">
+          <div class="w-2/4 flex flex-col justify-center mx-auto md:pl-6 md:pr-20">       
+            <p class="text-2xl md:text-6xl mb-6 text-[#1A265C]">
                 ${data.description}
             </p>
-            <p class="font-semibold text-lg md:text-6xl italic text-[#617EFF]">
+            <p class="font-semibold text-2xl md:text-6xl italic text-[#617EFF]">
                 ${data.fullName}
             </p>
-            <p class="font-medium text-lg md:text-6xl italic text-[#617EFF]">
+            <p class="font-medium text-2xl md:text-6xl italic text-[#617EFF]">
                 ${data.work}
             </p>            
           </div>
